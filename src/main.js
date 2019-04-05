f21e6df01a74286d81607ea7659d1a26d59ebc1a
Viggo Petersen <viggo.petersen@sallinggroup.com>
2019-04-05 23:49:51 +0200

    Add firebase

--- src/main.js
+++ src/main.js
@@ -2,11 +2,34 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
 
+import './registerServiceWorker'
 import 'bootstrap-css-only';
 
-Vue.config.productionTip = false
+import firebase from 'firebase';
+
+var config = {
+  apiKey: process.env.VUE_APP_FIREBASE_KEY,
+  authDomain: 'swearjar.firebaseapp.com',
+  databaseURL: 'https://swearjar-4a26e.firebaseio.com',
+  storageBucket: 'swearjar-4a26e.appspot.com',
+};
+
+var firebaseApp = firebase.initializeApp(config);
+
+/*
+var ref = firebaseApp.database().ref('known_swears');
+
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+*/
+
+Vue.config.productionTip = false;
+
+Vue.prototype.$db = firebase.database();
 
 new Vue({
   router,
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,63 @@
 <template>
     <div class="container">
         <div class="row">
-            <h1 class="mx-auto">AUHack19! SwearJar</h1>
+            <div class="col-12">
+                <h1 class="mx-auto">AUHack19! SwearJar</h1>
+            </div>
+            <div class="col-12 d-flex justify-content-center">
+                <ul class="list-unstyled text-left" id="test">
+                    <li v-for="(swear, index) in swears">
+                        {{ index }} - {{ swear }}
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12">
+                <label for="inpSwear">Add new swear</label>
+                <input id="inpSwear" v-model="message">
+                <button @click="addSwear">Add swear</button>
+                <p class="bg-warning">{{ error }}</p>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {}
+import Vue from 'vue';
+
+export default {
+    data() {
+        return {
+            message: '',
+            error: '',
+            swears: [],
+            counter: 0,
+            ref: Vue.prototype.$db.ref('known_swears')
+        }
+    },
+    created() {
+        const _self = this;
+
+        this.ref.on("value", function(snapshot) {
+            _self.swears = [];
+            Object.keys(snapshot.val()).forEach(item => _self.swears.push(snapshot.val()[item]));
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+    },
+    methods: {
+        addSwear(event) {
+            if(this.message && !this.swears.includes(this.message)) {
+                this.ref.push(this.message);
+                this.error = '';
+            }
+            else if(this.message === '') {
+                this.error = 'Please type something.';
+            }
+            else {
+                this.error = 'Swear already exists!'
+            }
+            this.message = '';
+        }
+    }
+}
 </script>
