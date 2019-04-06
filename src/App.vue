5cf900c460cff48d716cc137d88d1d041fbea973
Viggo Petersen <viggo.petersen@sallinggroup.com>
2019-04-06 15:21:25 +0200

    Move header and add click-to-home
    - move header to left
    - change text of header
    - add onclick on header to go to home page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1 class="mx-auto header-text">AUHack19! SwearJar</h1>
+    <h1 class="text-left pl-3 header-text" @click="$router.push('/')">SwearJar</h1>
     <router-view/>
   </div>
 </template>
@@ -17,6 +17,7 @@
 
 .header-text {
   background-color: darkgray;
+  cursor: pointer;
   color: white;
   box-shadow: 0 -5px 25px 5px black;
 }
